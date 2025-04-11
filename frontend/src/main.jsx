4895781aadbb995e8b9c7734729b4b81e3dd1a3a
grzx3 <grzx2brzeczyszczykiewicz@gmail.com>
2025-04-11 10:10:34 +0300

    CartPage implementation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import PaymentPage from './Components/PaymentPage.jsx'
+import CartPage from './Components/CartPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PaymentPage />
+    <CartPage />
   </StrictMode>,
 )
