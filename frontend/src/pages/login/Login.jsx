1de3b08a4be6765af24e7d10230eedc9c0026e5c
berkay saylak <berksa-27@hotmail.com>
2025-04-13 09:54:56 +0000

    Edit Login.jsx

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -21,7 +21,7 @@ function LoginForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8080/api/login", {
+      const response = await fetch("http://localhost:8080/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
