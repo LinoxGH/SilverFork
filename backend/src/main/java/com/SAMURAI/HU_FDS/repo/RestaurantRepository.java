01b498931480b815864702cb98335d87550bedec
FIRAT <firatt99@hotmail.com>
2025-04-25 20:12:53 +0300

    Some updates and Search&Browse

--- backend/src/main/java/com/SAMURAI/HU_FDS/repo/RestaurantRepository.java
+++ backend/src/main/java/com/SAMURAI/HU_FDS/repo/RestaurantRepository.java
@@ -4,6 +4,7 @@ import com.SAMURAI.HU_FDS.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,14 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findById(Long id);
 
     Optional<Restaurant> findByName(String name);
+
+    List<Restaurant> findByNameContainingIgnoreCase(String name);
+    List<Restaurant> findByCategoryIgnoreCase(String category);
+    List<Restaurant> findByCuisineIgnoreCase(String cuisine);
+    List<Restaurant> findByRatingGreaterThanEqual(Double rating);
+    List<Restaurant> findByCategoryIgnoreCaseAndCuisineIgnoreCase(String category, String cuisine);
+    List<Restaurant> findByCategoryIgnoreCaseAndRatingGreaterThanEqual(String category, Double rating);
+    List<Restaurant> findByCuisineIgnoreCaseAndRatingGreaterThanEqual(String cuisine, Double rating);
+
+    List<Restaurant> findByCategoryIgnoreCaseAndCuisineIgnoreCaseAndRatingGreaterThanEqual(String category, String cuisine, Double rating);
 }
