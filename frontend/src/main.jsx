0b267d1b149a1b9e5d7857bd0b82effe156637c6
Muhammed Ali Uzun <muhammed.uzun@hacettepe.edu.tr>
2025-04-29 20:37:06 +0300

    admin dashboard implementation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,8 @@ import ManageUser from './pages/user/ManageUser.jsx';
 import Favourites from './pages/favourites/Favourites.jsx';
 import ProductDetail from './pages/product/Product.jsx';
 import ShowSearchResult from './pages/searchResult/SearchResult.jsx';
+import AdminDashboard from './pages/adminDashboard/adminDashboard.jsx';
+import AdminManageUser from "./pages/adminManageUser/adminManageUser.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -28,6 +30,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/favourites" element={<Favourites />} />
         <Route path="/product" element={<ProductDetail />} />
         <Route path="/search-result" element={<ShowSearchResult />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin-dashboard/:username" element={<AdminManageUser />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
